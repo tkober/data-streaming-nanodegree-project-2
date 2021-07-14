bb003e43d4311c7ceeca4b241a136b26c796d2e1
Thorsten Kober <thorsten.kober@softgarden.de>
2021-07-14 10:07:41 +0200

    Created customer schema

--- project/starter/sparkpyrediskafkastreamtoconsole.py
+++ project/starter/sparkpyrediskafkastreamtoconsole.py
@@ -3,27 +3,31 @@ from pyspark.sql.functions import from_json, to_json, col, unbase64, base64, spl
 from pyspark.sql.types import StructField, StructType, StringType, BooleanType, ArrayType, DateType
 
 # TODO: create a StructType for the Kafka redis-server topic which has all changes made to Redis - before Spark 3.0.0, schema inference is not automatic
-redisSchema = StructType(
-    [
-        StructField("key", StringType()),
-        StructField("value", StringType()),
-        StructField("expiredType", StringType()),
-        StructField("expiredValue", StringType()),
-        StructField("existType", StringType()),
-        StructField("ch", StringType()),
-        StructField("incr", BooleanType()),
-        StructField("zSetEntries", ArrayType( \
-            StructType([
-                StructField("element", StringType()), \
-                StructField("score", StringType()) \
-                ])) \
-                    )
+redisSchema = StructType([
+    StructField('key', StringType()),
+    StructField('value', StringType()),
+    StructField('expiredType', StringType()),
+    StructField('expiredValue', StringType()),
+    StructField('existType', StringType()),
+    StructField('ch', StringType()),
+    StructField('incr', BooleanType()),
+    StructField('zSetEntries', ArrayType( \
+        StructType([
+            StructField('element', StringType()), \
+            StructField('score', StringType()) \
+            ])) \
+                )
 
-    ]
-)
+])
 
 # TODO: create a StructType for the Customer JSON that comes from Redis- before Spark 3.0.0, schema inference is not automatic
-
+# Example {"customerName":"Sam Test","email":"sam.test@test.com","phone":"8015551212","birthDay":"2001-01-03"}
+customerSchema = StructType([
+    StructField('customerName', StringType()),
+    StructField('email', StringType()),
+    StructField('phone', StringType()),
+    StructField('birthDay', StringType())
+])
 
 # TODO: create a StructType for the Kafka stedi-events topic which has the Customer Risk JSON that comes from Redis- before Spark 3.0.0, schema inference is not automatic
 
