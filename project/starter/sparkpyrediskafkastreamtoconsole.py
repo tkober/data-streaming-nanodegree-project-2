93c902589a413e565e6d183644cdaa42fb193934
Thorsten Kober <thorsten.kober@softgarden.de>
2021-07-14 10:12:53 +0200

    Created stedi-event Schema

--- project/starter/sparkpyrediskafkastreamtoconsole.py
+++ project/starter/sparkpyrediskafkastreamtoconsole.py
@@ -1,6 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, to_json, col, unbase64, base64, split, expr
-from pyspark.sql.types import StructField, StructType, StringType, BooleanType, ArrayType, DateType
+from pyspark.sql.types import StructField, StructType, StringType, BooleanType, ArrayType, DateType, FloatType
 
 # TODO: create a StructType for the Kafka redis-server topic which has all changes made to Redis - before Spark 3.0.0, schema inference is not automatic
 redisSchema = StructType([
@@ -11,17 +11,22 @@ redisSchema = StructType([
     StructField('existType', StringType()),
     StructField('ch', StringType()),
     StructField('incr', BooleanType()),
-    StructField('zSetEntries', ArrayType( \
+    StructField('zSetEntries', ArrayType(
         StructType([
-            StructField('element', StringType()), \
-            StructField('score', StringType()) \
-            ])) \
-                )
-
+            StructField('element', StringType()),
+            StructField('score', StringType())
+        ]))
+    )
 ])
 
 # TODO: create a StructType for the Customer JSON that comes from Redis- before Spark 3.0.0, schema inference is not automatic
-# Example {"customerName":"Sam Test","email":"sam.test@test.com","phone":"8015551212","birthDay":"2001-01-03"}
+# Example
+# {
+#   "customerName": "Sam Test",
+#   "email": "sam.test@test.com",
+#   "phone":"8015551212",
+#   "birthDay":"2001-01-03"
+# }
 customerSchema = StructType([
     StructField('customerName', StringType()),
     StructField('email', StringType()),
@@ -30,6 +35,17 @@ customerSchema = StructType([
 ])
 
 # TODO: create a StructType for the Kafka stedi-events topic which has the Customer Risk JSON that comes from Redis- before Spark 3.0.0, schema inference is not automatic
+# Example
+# {
+#   "customer":"Jason.Mitra@test.com",
+#   "score":7.0,
+#   "riskDate":"2020-09-14T07:54:06.417Z"
+# }
+stediEventSchema = StructType([
+    StructField('customer', StringType()),
+    StructField('score', FloatType()),
+    StructField('riskDate', DateType())
+])
 
 # TODO: create a spark application object
 
