50cf1f96eae6a2121ea16d26797dcd179a9e5508
Thorsten Kober <thorsten.kober@softgarden.de>
2021-07-14 11:49:16 +0200

    Formatting

--- project/starter/sparkpyeventskafkastreamtoconsole.py
+++ project/starter/sparkpyeventskafkastreamtoconsole.py
@@ -8,11 +8,11 @@ spark.sparkContext.setLogLevel('WARN')
 
 # TODO: using the spark application object, read a streaming dataframe from the Kafka topic stedi-events as the source
 # Be sure to specify the option that reads all the events from the topic including those that were published before you started the spark stream
-stediEventsRawDf = spark.readStream\
-    .format('kafka')\
-    .option('subscribe', STEDI_EVENTS_TOPIC)\
-    .option('kafka.bootstrap.servers', KAFKA_BROKERS_STRING)\
-    .option('startingOffsets', 'earliest')\
+stediEventsRawDf = spark.readStream \
+    .format('kafka') \
+    .option('subscribe', STEDI_EVENTS_TOPIC) \
+    .option('kafka.bootstrap.servers', KAFKA_BROKERS_STRING) \
+    .option('startingOffsets', 'earliest') \
     .load()
 
 # TODO: cast the value column in the streaming dataframe as a STRING
@@ -33,8 +33,8 @@ stediEventsDf = stediEventsRawDf.selectExpr('cast(value as string) value')
 # +------------+-----+-----------+
 #
 # storing them in a temporary view called CustomerRisk
-stediEventsDf.withColumn('value', from_json('value', stediEventSchema))\
-    .select(col('value.*'))\
+stediEventsDf.withColumn('value', from_json('value', stediEventSchema)) \
+    .select(col('value.*')) \
     .createOrReplaceTempView('CustomerRisk')
 
 # TODO: execute a sql statement against a temporary view, selecting the customer and the score from the temporary view, creating a dataframe called customerRiskStreamingDF
@@ -52,8 +52,8 @@ customerRiskStreamingDf = spark.sql('SELECT customer, score FROM CustomerRisk')
 # Run the python script by running the command from the terminal:
 # /home/workspace/submit-event-kafka-streaming.sh
 # Verify the data looks correct
-customerRiskStreamingDf.writeStream\
-    .outputMode('append')\
-    .format('console')\
-    .start()\
+customerRiskStreamingDf.writeStream \
+    .outputMode('append') \
+    .format('console') \
+    .start() \
     .awaitTermination()
