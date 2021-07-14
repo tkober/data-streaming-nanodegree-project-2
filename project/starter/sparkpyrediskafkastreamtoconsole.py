0ddb7f69888dbc3cb3d62d320ea02a03f2eca70c
Thorsten Kober <thorsten.kober@softgarden.de>
2021-07-14 10:03:47 +0200

    TO-DO --> TODO for IDE highlighting

--- project/starter/sparkpyrediskafkastreamtoconsole.py
+++ project/starter/sparkpyrediskafkastreamtoconsole.py
@@ -2,7 +2,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, to_json, col, unbase64, base64, split, expr
 from pyspark.sql.types import StructField, StructType, StringType, BooleanType, ArrayType, DateType
 
-# TO-DO: create a StructType for the Kafka redis-server topic which has all changes made to Redis - before Spark 3.0.0, schema inference is not automatic
+# TODO: create a StructType for the Kafka redis-server topic which has all changes made to Redis - before Spark 3.0.0, schema inference is not automatic
 redisSchema = StructType(
     [
         StructField("key", StringType()),
@@ -22,21 +22,21 @@ redisSchema = StructType(
     ]
 )
 
-# TO-DO: create a StructType for the Customer JSON that comes from Redis- before Spark 3.0.0, schema inference is not automatic
+# TODO: create a StructType for the Customer JSON that comes from Redis- before Spark 3.0.0, schema inference is not automatic
 
 
-# TO-DO: create a StructType for the Kafka stedi-events topic which has the Customer Risk JSON that comes from Redis- before Spark 3.0.0, schema inference is not automatic
+# TODO: create a StructType for the Kafka stedi-events topic which has the Customer Risk JSON that comes from Redis- before Spark 3.0.0, schema inference is not automatic
 
-# TO-DO: create a spark application object
+# TODO: create a spark application object
 
-# TO-DO: set the spark log level to WARN
+# TODO: set the spark log level to WARN
 
-# TO-DO: using the spark application object, read a streaming dataframe from the Kafka topic redis-server as the source
+# TODO: using the spark application object, read a streaming dataframe from the Kafka topic redis-server as the source
 # Be sure to specify the option that reads all the events from the topic including those that were published before you started the spark stream
 
-# TO-DO: cast the value column in the streaming dataframe as a STRING 
+# TODO: cast the value column in the streaming dataframe as a STRING 
 
-# TO-DO:; parse the single column "value" with a json object in it, like this:
+# TODO:; parse the single column "value" with a json object in it, like this:
 # +------------+
 # | value      |
 # +------------+
@@ -68,10 +68,10 @@ redisSchema = StructType(
 #
 # storing them in a temporary view called RedisSortedSet
 
-# TO-DO: execute a sql statement against a temporary view, which statement takes the element field from the 0th element in the array of structs and create a column called encodedCustomer
+# TODO: execute a sql statement against a temporary view, which statement takes the element field from the 0th element in the array of structs and create a column called encodedCustomer
 # the reason we do it this way is that the syntax available select against a view is different than a dataframe, and it makes it easy to select the nth element of an array in a sql column
 
-# TO-DO: take the encodedCustomer column which is base64 encoded at first like this:
+# TODO: take the encodedCustomer column which is base64 encoded at first like this:
 # +--------------------+
 # |            customer|
 # +--------------------+
@@ -87,16 +87,16 @@ redisSchema = StructType(
 #
 # with this JSON format: {"customerName":"Sam Test","email":"sam.test@test.com","phone":"8015551212","birthDay":"2001-01-03"}
 
-# TO-DO: parse the JSON in the Customer record and store in a temporary view called CustomerRecords
+# TODO: parse the JSON in the Customer record and store in a temporary view called CustomerRecords
 
-# TO-DO: JSON parsing will set non-existent fields to null, so let's select just the fields we want, where they are not null as a new dataframe called emailAndBirthDayStreamingDF
+# TODO: JSON parsing will set non-existent fields to null, so let's select just the fields we want, where they are not null as a new dataframe called emailAndBirthDayStreamingDF
 
-# TO-DO: from the emailAndBirthDayStreamingDF dataframe select the email and the birth year (using the split function)
+# TODO: from the emailAndBirthDayStreamingDF dataframe select the email and the birth year (using the split function)
 
-# TO-DO: Split the birth year as a separate field from the birthday
-# TO-DO: Select only the birth year and email fields as a new streaming data frame called emailAndBirthYearStreamingDF
+# TODO: Split the birth year as a separate field from the birthday
+# TODO: Select only the birth year and email fields as a new streaming data frame called emailAndBirthYearStreamingDF
 
-# TO-DO: sink the emailAndBirthYearStreamingDF dataframe to the console in append mode
+# TODO: sink the emailAndBirthYearStreamingDF dataframe to the console in append mode
 # 
 # The output should look like this:
 # +--------------------+-----               
